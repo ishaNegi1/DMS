a80d0b5bc92f6178374ea91aeae523438e76d916
Isha Negi <negi.isha98@gmail.com>
2024-10-11 02:00:40 +0530

    update component

--- frontend/src/components/allComponents.jsx
+++ frontend/src/components/allComponents.jsx
@@ -3,8 +3,6 @@ import Button0 from './Button0'
 import Footer from './Footer'
 import Vector0 from './Vector0'
 import Vector1 from './Vector1'
-import Create from './Create'
-import Upload from './Upload'
 import Loader from './Loader'
 
-export{Button, Button0, Footer, Vector0, Vector1, Create, Upload, Loader}
+export{Button, Button0, Footer, Vector0, Vector1, Loader}
